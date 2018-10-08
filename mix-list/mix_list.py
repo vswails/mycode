7da869d304a190c99e318e9240a349acb0ff6f21
Ginny Swails <gineric1@hotmail.com>
2018-10-08 17:21:16 -0400

    mix list code

---
+++ mix-list/mix_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# mix list lab
+my_list = [ "192.168.0.5", 5060, "UP" ]
+print("The first item in the list (IP): " + my_list[0] )
+print("The second item in the list (port): " + str(my_list[1]) )
+print("The last item in the list (state): " + my_list[2] )
+new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
